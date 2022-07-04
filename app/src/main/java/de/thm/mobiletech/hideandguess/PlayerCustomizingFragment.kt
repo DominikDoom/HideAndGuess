204fef2e6cf1bfb1b808af4097159cf174388253
Serdar Tuzcu <sedooxyt@gmail.com>
2022-07-04 20:42:21 +0200

    used generic DataBindingFragment

--- app/src/main/java/de/thm/mobiletech/hideandguess/PlayerCustomizingFragment.kt
+++ app/src/main/java/de/thm/mobiletech/hideandguess/PlayerCustomizingFragment.kt
@@ -1,46 +1,23 @@
 package de.thm.mobiletech.hideandguess
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import de.thm.mobiletech.hideandguess.databinding.FragmentPlayerCustomizingBinding
+import de.thm.mobiletech.hideandguess.util.DataBindingFragment
 import java.util.*
-import kotlin.collections.ArrayList
 
 
-class PlayerCustomizingFragment : Fragment() {
+class PlayerCustomizingFragment : DataBindingFragment<FragmentPlayerCustomizingBinding>(R.layout.fragment_player_customizing) {
 
-    private lateinit var binding: FragmentPlayerCustomizingBinding
-    private lateinit var navController: NavController
     private var playerImages: ArrayList<Int> = ArrayList<Int>(Arrays.asList(R.drawable.amongus_player_blue, R.drawable.amongus_player_red, R.drawable.amongus_player_green, R.drawable.amongus_player_yellow))
     private var playerImagesCursor: Int = 0
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate layout using data binding
-        binding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_player_customizing, container, false
-        )
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.context = this // Set binding variable used in layout
-        return binding.root
+    override fun setBindingContext() {
+        binding.context = this
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Find and assign navController
-        val navHostFragment = requireActivity().supportFragmentManager
-            .findFragmentById(R.id.navHost) as NavHostFragment
-        navController = navHostFragment.navController
-
         binding.imageCustomizingPlayer.setImageResource(R.drawable.amongus_player_blue)
     }
 
