fe02424a3f586df5c2e2eb30ac05365b0bf4ae0e
Serdar Tuzcu <sedooxyt@gmail.com>
2022-07-05 12:08:25 +0200

    added basic lobby fragment

---
+++ app/src/main/java/de/thm/mobiletech/hideandguess/LobbyFragment.kt
@@ -0,0 +1,11 @@
+package de.thm.mobiletech.hideandguess
+
+import de.thm.mobiletech.hideandguess.databinding.FragmentLobbyBinding
+import de.thm.mobiletech.hideandguess.util.DataBindingFragment
+
+class LobbyFragment : DataBindingFragment<FragmentLobbyBinding>(R.layout.fragment_lobby) {
+    override fun setBindingContext() {
+        binding.context = this
+    }
+
+}
