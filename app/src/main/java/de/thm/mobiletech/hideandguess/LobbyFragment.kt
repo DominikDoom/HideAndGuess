ece6d63e9b89b2a068ca1b5996726cce5202e446
Serdar Tuzcu <sedooxyt@gmail.com>
2022-07-05 22:18:27 +0200

    created cardview_user.xml, changed fragment layout and added addPlayer function

--- app/src/main/java/de/thm/mobiletech/hideandguess/LobbyFragment.kt
+++ app/src/main/java/de/thm/mobiletech/hideandguess/LobbyFragment.kt
@@ -1,5 +1,9 @@
 package de.thm.mobiletech.hideandguess
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import de.thm.mobiletech.hideandguess.databinding.FragmentLobbyBinding
 import de.thm.mobiletech.hideandguess.util.DataBindingFragment
 
@@ -8,7 +12,18 @@ class LobbyFragment : DataBindingFragment<FragmentLobbyBinding>(R.layout.fragmen
         binding.context = this
     }
 
-    fun addPlayer(user: User) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // addPlayer(yourself)
+    }
 
+    fun addPlayer(user: User) {
+        val grid = binding.gridlayoutPlayers
+        val view = layoutInflater.inflate(R.layout.cardview_user, grid, false)
+        val text = view.findViewById<TextView>(R.id.textViewUser)
+        val image = view.findViewById<ImageView>(R.id.imageViewUser)
+        text.text = user.username
+        image.setImageResource(R.drawable.example_user_image1)
+        grid.addView(view)
     }
 }
