6863d8ea0f688871ed6156692dfae6e126253bd1
rabitem <f.huisinga@arcor.de>
2022-07-14 22:19:36 +0200

    fix?

--- app/src/main/java/de/thm/mobiletech/hideandguess/LobbyFragment.kt
+++ app/src/main/java/de/thm/mobiletech/hideandguess/LobbyFragment.kt
@@ -165,18 +165,19 @@ class LobbyFragment : DataBindingFragment<FragmentLobbyBinding>(R.layout.fragmen
 
     private suspend fun evaluateLobbyState(lobbyState: LobbyState) {
         if ((lastLobbyState == null || lastLobbyState == LobbyState.NOT_IN_GAME) && lobbyState == LobbyState.PAINTING_CHOOSE) {
+            lastLobbyState = lobbyState
             requestPainter()
         } else if (lastLobbyState == lobbyState) {
             // do nothing
         } else if (lastLobbyState == LobbyState.PAINTING_CHOOSE && lobbyState == LobbyState.PAINTING) {
             // do nothing
+            lastLobbyState = lobbyState
         } else if (lastLobbyState == LobbyState.PAINTING && lobbyState == LobbyState.GUESSING) {
+            lastLobbyState = lobbyState
             (requireActivity() as MainActivity).hideOtherPaintingDialog()
             val action = LobbyFragmentDirections.actionLobbyFragmentToGuessFragment()
             navController.navigate(action)
         }
-
-        lastLobbyState = lobbyState
     }
 
     data class LobbyInfo(
