d257cf398fcf4314fba35e5462a3b559b90d98d9
rabitem <f.huisinga@arcor.de>
2022-07-14 22:02:24 +0200

    Remove evil slashes

--- app/src/main/java/de/thm/mobiletech/hideandguess/rest/services/GameService.kt
+++ app/src/main/java/de/thm/mobiletech/hideandguess/rest/services/GameService.kt
@@ -6,7 +6,7 @@ import de.thm.mobiletech.hideandguess.rest.Result
 suspend fun RestClient.submitPaintingChoice(query: String, lobbyId: Int): Result<Int> {
 
     try {
-        return this.postRequest("/imageChosen/$lobbyId/$query")
+        return this.postRequest("imageChosen/$lobbyId/$query")
     } catch (e: Exception) {
         throw Exception("Error while submitting painting choice")
     }
